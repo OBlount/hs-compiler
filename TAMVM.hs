ac73cbb1238c89b08bfe1bc7e55b037cf162cd76
Owen Blount <oblount077@gmail.com>
2024-10-14 23:26:20 +0100

    added code gen AST -> instruction set

--- TAMVM.hs
+++ TAMVM.hs
@@ -1,15 +1,28 @@
 module TAMVM where
 
-type Stack = [Int]
+import Parser (AST(..), BinaryOperator(..), UnaryOperator(..))
+
+type Stack = [Integer]
 
 data TAMInst
-  = LOADL Int
+  = LOADL Integer
   | ADD
   | SUB
   | MUL
   | DIV
   | NEG
   deriving (Show)
+
+expCode :: AST -> [TAMInst]
+expCode (LitInteger x)        = [LOADL x]
+expCode (BinOp op left right) =
+  let left'  = expCode left
+      right' = expCode right
+  in case op of
+    Addition       -> left' ++ right' ++ [ADD]
+    Subtraction    -> left' ++ right' ++ [SUB]
+    Multiplication -> left' ++ right' ++ [MUL]
+    Division       -> left' ++ right' ++ [DIV]
 
 execute :: Stack -> TAMInst -> Maybe Stack
 execute stack (LOADL x) = Just (x:stack)
@@ -28,7 +41,19 @@ executeTAM stack (i:is) = case execute stack i of
 
 main :: IO ()
 main = do
+  -- Test instruction set
+  putStrLn "test 1: test execution of instruction set"
   let instructions = [LOADL 3, LOADL 5, MUL]
   case executeTAM [] instructions of
     Just res   -> print res
     Nothing    -> putStrLn "error - failed to execute TAM"
+  -- Test code generation
+  putStrLn "test 2: test code generation"
+  let tree = BinOp Multiplication (LitInteger 3) (BinOp Addition (LitInteger 3) (LitInteger 2))
+  let generatedCode = expCode tree
+  print generatedCode
+  -- Test code generation evaluation
+  putStrLn "test 3: test code generation evaluation"
+  case executeTAM [] generatedCode of
+    Just res   -> print res
+    Nothing    -> putStrLn "error - failed to execute generated TAM"
