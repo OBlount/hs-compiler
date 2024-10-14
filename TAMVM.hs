ed2701a6eec5b9a144e0b69bd97087291f79a0ac
Owen Blount <oblount077@gmail.com>
2024-10-14 22:13:50 +0100

    able to read TAM instruction sets

--- TAMVM.hs
+++ TAMVM.hs
@@ -12,11 +12,23 @@ data TAMInst
   deriving (Show)
 
 execute :: Stack -> TAMInst -> Maybe Stack
-execute stack (LOADL x) = undefined
+execute stack (LOADL x) = Just (x:stack)
+execute (x:y:stack) ADD = Just ((x+y)    :stack)
+execute (x:y:stack) SUB = Just ((x-y)    :stack)
+execute (x:y:stack) MUL = Just ((x*y)    :stack)
+execute (x:y:stack) DIV = Just ((x`div`y):stack)
+execute (x:stack)   NEG = Just ((-x)     :stack)
+execute _ _             = Nothing
+
+executeTAM :: Stack -> [TAMInst] -> Maybe Stack
+executeTAM stack []     = Just stack
+executeTAM stack (i:is) = case execute stack i of
+                            Just stack' -> executeTAM stack' is
+                            Nothing     -> Nothing
 
 main :: IO ()
 main = do
-  let stack = execute [] (LOADL 123)
-  case stack of
-    Just stack -> print stack
-    Nothing    -> putStrLn "error"
+  let instructions = [LOADL 3, LOADL 5, MUL]
+  case executeTAM [] instructions of
+    Just res   -> print res
+    Nothing    -> putStrLn "error - failed to execute TAM"
