57c43c52126491cd548d521c018569e9581fbac0
Owen Blount <oblount077@gmail.com>
2024-10-14 23:51:32 +0100

    moved code gen to seperate module and modified Main to compArith

--- Main.hs
+++ Main.hs
@@ -1,9 +1,14 @@
 module Main where
 
+import TAMVM (TAMInst(..))
 import Scanner (scan)
 import Parser (parse)
-import Evaluator (eval)
+import CodeGenerator (expCode)
+
+compArith :: String -> [TAMInst]
+compArith src = expCode $ parse $ scan src
 
 main :: IO ()
 main = do
-  print $ eval $ parse $ scan "3 + 5"
+  let src = "3 * (3 * 5)"
+  print $ compArith src
--- TAMVM.hs
+++ TAMVM.hs
@@ -1,7 +1,5 @@
 module TAMVM where
 
-import Parser (AST(..), BinaryOperator(..), UnaryOperator(..))
-
 type Stack = [Integer]
 
 data TAMInst
@@ -13,16 +11,11 @@ data TAMInst
   | NEG
   deriving (Show)
 
-expCode :: AST -> [TAMInst]
-expCode (LitInteger x)        = [LOADL x]
-expCode (BinOp op left right) =
-  let left'  = expCode left
-      right' = expCode right
-  in case op of
-    Addition       -> left' ++ right' ++ [ADD]
-    Subtraction    -> left' ++ right' ++ [SUB]
-    Multiplication -> left' ++ right' ++ [MUL]
-    Division       -> left' ++ right' ++ [DIV]
+executeTAM :: Stack -> [TAMInst] -> Stack
+executeTAM stack []     = stack
+executeTAM stack (i:is) = case execute stack i of
+                            Just stack' -> executeTAM stack' is
+                            Nothing     -> error $ "TAMVM: execution failed on instruction: " ++ show i
 
 execute :: Stack -> TAMInst -> Maybe Stack
 execute stack (LOADL x) = Just (x:stack)
@@ -33,27 +26,7 @@ execute (x:y:stack) DIV = Just ((x`div`y):stack)
 execute (x:stack)   NEG = Just ((-x)     :stack)
 execute _ _             = Nothing
 
-executeTAM :: Stack -> [TAMInst] -> Maybe Stack
-executeTAM stack []     = Just stack
-executeTAM stack (i:is) = case execute stack i of
-                            Just stack' -> executeTAM stack' is
-                            Nothing     -> Nothing
-
 main :: IO ()
 main = do
-  -- Test instruction set
-  putStrLn "test 1: test execution of instruction set"
   let instructions = [LOADL 3, LOADL 5, MUL]
-  case executeTAM [] instructions of
-    Just res   -> print res
-    Nothing    -> putStrLn "error - failed to execute TAM"
-  -- Test code generation
-  putStrLn "test 2: test code generation"
-  let tree = BinOp Multiplication (LitInteger 3) (BinOp Addition (LitInteger 3) (LitInteger 2))
-  let generatedCode = expCode tree
-  print generatedCode
-  -- Test code generation evaluation
-  putStrLn "test 3: test code generation evaluation"
-  case executeTAM [] generatedCode of
-    Just res   -> print res
-    Nothing    -> putStrLn "error - failed to execute generated TAM"
+  print $ executeTAM [] instructions
