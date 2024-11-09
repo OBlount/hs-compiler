d834381b46f114185956bc5b70784f566d859a97
Owen Blount <oblount077@gmail.com>
2024-11-09 23:09:39 +0000

    .tam files are correctly processed

--- Main.hs
+++ Main.hs
@@ -24,7 +24,10 @@ main = do
             [(ast, endString)] -> do
               if endString == "" || endString == "\n" then writeInstructions (compile ast) (name ++ ".tam")
                                                       else error "[ERROR] - Unable to parse program"
-        ".tam" -> undefined
+        ".tam" -> do
+                    let instructions = readInstructions contents
+                    let initialState = TAMState { tsCode = instructions, tsCounter = 0, tsStack = [] }
+                    execute executeProgram initialState
         _      -> error "Please provide a .mt or .tam file"
     []         -> error "Program usage: \n\t- ./mtc [filename].mt\n\t- ./mtc [filename.tam]"
 
@@ -32,3 +35,30 @@ writeInstructions :: [Instruction] -> FilePath -> IO ()
 writeInstructions is path = do
   let content = unlines (map show is)
   writeFile path content
+
+readInstructions :: String -> [Instruction]
+readInstructions input =
+  case map readInstruction (lines input) of
+    list -> [i | i <- list]
+
+readInstruction :: String -> Instruction
+readInstruction line = case words line of
+  ["LOADL", n]     -> LOADL (read n)
+  ["STORE", n]     -> STORE (read n)
+  ["LOAD", n]      -> LOAD (read n)
+  ["GETINT"]       -> GETINT
+  ["PUTINT"]       -> PUTINT
+  ["HALT"]         -> HALT
+  ["ADD"]          -> ADD
+  ["MUL"]          -> MUL
+  ["LSS"]          -> LSS
+  ["GRT"]          -> GRT
+  ["NOT"]          -> NOT
+  ["JUMP", l]      -> JUMP (removeQuotes l)
+  ["JUMPIFZ", l]   -> JUMPIFZ (removeQuotes l)
+  ["Label", l]     -> Label (removeQuotes l)
+  _                -> error "[ERROR] - Something went wrong reading your file"
+
+removeQuotes :: String -> String
+removeQuotes str = if head str == '"' && last str == '"' then tail (init str)
+                                                         else str
